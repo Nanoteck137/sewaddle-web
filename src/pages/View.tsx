fd5bbc1447af13cac38a717ecaab168974c91fad
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-02-06 12:31:49 +0100

    Working on pages

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,19 +1,55 @@
-import { useParams } from "@solidjs/router";
+import { useParams, useSearchParams } from "@solidjs/router";
 import { createQuery } from "@tanstack/solid-query";
-import { For, Match, Show, Switch } from "solid-js";
+import { Match, Show, Switch, createEffect, createMemo } from "solid-js";
 import { getChapterById } from "../api/chapter";
 
 const View = () => {
   const params = useParams<{ id: string }>();
+
+  const [searchParams, setSearchParams] = useSearchParams<{ page?: string }>();
 
   const chapter = createQuery(() => ({
     queryKey: ["chapters", params.id],
     queryFn: () => getChapterById(params.id),
   }));
 
+  createEffect(() => {
+    console.log(chapter.data?.pages.length);
+  });
+
+  createEffect(() => {
+    console.log(searchParams.page);
+    if (!searchParams.page || searchParams.page == "0") {
+      setSearchParams({ page: "1" });
+    }
+  });
+
+  const page = createMemo(() => {
+    const page = parseInt(searchParams.page || "0") - 1;
+    return chapter.data?.pages[page];
+  });
+
+  const nextPage = () => {
+    if (!chapter.data) return;
+
+    const currentPage = searchParams.page ? parseInt(searchParams.page) : 1;
+    const nextPage = currentPage + 1;
+    if (nextPage < chapter.data.pages.length + 1) {
+      setSearchParams({ page: (currentPage + 1).toString() });
+    }
+  };
+
   return (
     <>
       <p>View Page: {params.id}</p>
+      <p>View Page: {searchParams.page}</p>
+      <button
+        onClick={() => {
+          nextPage();
+        }}
+      >
+        Click
+      </button>
 
       <Switch>
         <Match when={chapter.isLoading}>
@@ -38,13 +74,9 @@ const View = () => {
             <a href={`/view/${chapter.data?.prevChapterId}`}>Prev Chapter</a>
           </Show>
 
-          <For each={chapter.data?.pages}>
-            {(page) => {
-              return (
-                <img loading="lazy" src={page} alt="Page" draggable="false" />
-              );
-            }}
-          </For>
+          <Show when={page()}>
+            <img src={page()} alt="Page" />
+          </Show>
         </Match>
       </Switch>
     </>
