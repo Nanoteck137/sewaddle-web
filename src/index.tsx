ed23c6dda25a420927d75129de90e3ee004d1e34
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-04-22 00:19:27 +0200

    Added default layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "solid-js/web";
 
 import { Route, Router } from "@solidjs/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { Component, JSX } from "solid-js";
 import { AuthHandler } from "./AuthHandler";
 import { ApiClientProvider } from "./context/ApiClientContext";
 import { AuthProvider } from "./context/AuthContext";
@@ -19,18 +20,32 @@ const queryClient = new QueryClient();
 const authHandler = new AuthHandler();
 const apiClient = new ApiClient("http://10.28.28.6:3000", authHandler);
 
+const DefaultLayout: Component<{ children?: JSX.Element }> = (props) => {
+  return (
+    <>
+      <header class="h-14 bg-red-200">
+        <a class="text-3xl font-semibold" href="/">
+          Sewaddle
+        </a>
+      </header>
+      <main>{props.children}</main>
+    </>
+  );
+};
+
 render(
   () => (
     <QueryClientProvider client={queryClient}>
       <AuthProvider authHandler={authHandler}>
         <ApiClientProvider client={apiClient}>
           <Router>
-            <Route path="/">
+            <Route path="/" component={DefaultLayout}>
               <Route path="/" component={Home} />
               <Route path="/serie/:id" component={Serie} />
-              <Route path="/view/:serieId/:chapterNumber" component={View} />
-              <Route path="/login" component={Login} />
             </Route>
+
+            <Route path="/view/:serieId/:chapterNumber" component={View} />
+            <Route path="/login" component={Login} />
           </Router>
         </ApiClientProvider>
       </AuthProvider>
