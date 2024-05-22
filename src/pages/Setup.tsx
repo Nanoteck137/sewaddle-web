0e9f4372c80b8fa81dbd686712860dfb68f81ae0
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-05-22 15:58:24 +0200

    Running setup

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -1,32 +1,49 @@
 import { createSignal } from "solid-js";
+import { useApiClient } from "../context/ApiClientContext";
+import { PostSystemSetupBody } from "../lib/models/apiGen";
 
 const Setup = () => {
+  const apiClient = useApiClient();
+
   const [page, setPage] = createSignal(0);
 
-  const nextPage = () => {
-    setPage((prev) => prev + 1);
-  };
+  let userForm!: HTMLFormElement;
 
-  const prevPage = () => {
+  function nextPage() {
+    setPage((prev) => prev + 1);
+  }
+
+  function prevPage() {
     setPage((prev) => prev - 1);
-  };
+  }
+
+  function finishSetup() {
+    const userFormData = Object.fromEntries(new FormData(userForm));
+    const body = PostSystemSetupBody.parse(userFormData);
+    apiClient
+      .setup(body)
+      .then(() => (window.location.href = "/"))
+      .catch((e) => console.error(e));
+  }
 
   return (
     <>
       <div class={`${page() === 0 ? "" : "hidden"}`}>
         <p>Page 1: User Creation</p>
         <form
+          ref={userForm}
           class="flex flex-col gap-2"
           onSubmit={(e) => {
             e.preventDefault();
           }}
         >
           <label for="username">Username</label>
-          <input id="username" type="text" />
+          <input id="username" name="username" type="text" />
 
           <label for="new-password">Password</label>
           <input
             id="new-password"
+            name="password"
             type="password"
             autocomplete="new-password"
           />
@@ -34,6 +51,7 @@ const Setup = () => {
           <label for="passwordConfirm">Confirm Password</label>
           <input
             id="passwordConfirm"
+            name="passwordConfirm"
             type="password"
             autocomplete="new-password"
           />
@@ -44,6 +62,7 @@ const Setup = () => {
       <div class={`${page() === 1 ? "" : "hidden"}`}>
         <p>Page 2: Summary</p>
         <button onClick={prevPage}>Prev Page</button>
+        <button onClick={finishSetup}>Finish Setup</button>
       </div>
     </>
   );
