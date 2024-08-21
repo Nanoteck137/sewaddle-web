b5b5bb48cd6d203e16e3709ccac351d8d72322b9
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-08-21 03:20:53 +0200

    Some work on the series page ui

--- src/routes/(app)/series/[slug]/+page.svelte
+++ src/routes/(app)/series/[slug]/+page.svelte
@@ -1,17 +1,85 @@
 <script lang="ts">
+  import { Bookmark, BookPlus, EllipsisVertical } from "lucide-svelte";
+
   const { data } = $props();
+
+  let showPopupMenu = $state("");
 </script>
 
-<p>{data.serie.name}</p>
-<p>{data.serie.chapterCount}</p>
-<p>{data.serie.cover}</p>
+<div class="flex flex-col gap-2 px-5">
+  <div class="flex justify-center md:fixed md:h-full">
+    <div class="w-[280px]">
+      <img
+        class="h-[360px] w-[280px] rounded object-cover"
+        src={data.serie.cover}
+        alt=""
+      />
+      <div class="h-2"></div>
+      <p class="line-clamp-2 text-center font-bold">{data.serie.name}</p>
+    </div>
+  </div>
+  <div class="md:ml-4 md:pl-[280px]">
+    <div class="flex flex-col gap-2">
+      {#each data.chapters as chapter}
+        <div class="group relative flex items-center gap-2 border-b pb-1 pr-4">
+          <div class="flex flex-grow items-center gap-2">
+            <img
+              class="h-14 w-10 rounded object-cover"
+              loading="lazy"
+              src={chapter.coverArt}
+              alt="Chapter Cover"
+            />
+            <div class="flex flex-col gap-2">
+              <a
+                class="line-clamp-1 font-medium"
+                title={chapter.title}
+                href={`/view/${chapter.serieSlug}/${chapter.slug}`}
+              >
+                {chapter.title}
+              </a>
+              {#if chapter.user?.isMarked}
+                <p class="line-clamp-1 text-sm font-light">Read</p>
+              {/if}
+            </div>
+          </div>
+          <div>
+            <button
+              class="rounded-full p-1 hover:bg-black/20"
+              onclick={() => {
+                showPopupMenu = chapter.slug;
+              }}
+            >
+              <EllipsisVertical size="30" />
+            </button>
+          </div>
 
-<p>Bookmark: {data.serie.user?.bookmark?.chapterSlug}</p>
+          <div
+            class={`popup absolute right-7 top-full z-50 -translate-y-4 rounded bg-red-400 ${showPopupMenu === chapter.slug ? "" : "hidden"}`}
+          >
+            <div class="flex flex-col">
+              <button class="flex gap-1 rounded px-4 py-2 hover:bg-red-200">
+                <BookPlus />
+                <p>Mark as Read</p>
+              </button>
 
-<div class="flex flex-col">
-  {#each data.chapters as chapter}
-    <a href={`/view/${data.serie.slug}/${chapter.slug}`}
-      >{chapter.title} - {chapter.user?.isMarked}</a
-    >
-  {/each}
+              <button class="flex gap-1 rounded px-4 py-2 hover:bg-red-200">
+                <Bookmark />
+                <p>Set as Bookmark</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
 </div>
+
+{#if showPopupMenu !== ""}
+  <button
+    class="fixed inset-0 z-40 bg-purple-400/50"
+    onclick={() => {
+      showPopupMenu = "";
+    }}
+  >
+  </button>
+{/if}
